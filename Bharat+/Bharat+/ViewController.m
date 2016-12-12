02dd7f584a6e5859304e91304d4127189e611b3d
Eshwar M R <eshwar.mr@techendeavour.com>
2016-12-12 18:41:17 +0530

    small changes

--- Bharat+/Bharat+/ViewController.m
+++ Bharat+/Bharat+/ViewController.m
@@ -43,12 +43,12 @@
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    if([[[NSUserDefaults standardUserDefaults] objectForKey:@"Name"] isEqualToString:@""])
+    if([[[NSUserDefaults standardUserDefaults] objectForKey:@"Name"] isEqualToString:@""] || [[NSUserDefaults standardUserDefaults] objectForKey:@"Name"]==nil)
         [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(saveButtonSelected)]];
     else
     {
         [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithTitle:@"Post" style:UIBarButtonItemStylePlain target:self action:@selector(postButtonClicked)]];
-        [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithTitle:@"Menu" style:UIBarButtonItemStylePlain target:self action:@selector(saveButtonSelected)]];
+        [self.navigationItem setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithTitle:@"View Posts" style:UIBarButtonItemStylePlain target:self action:@selector(saveButtonSelected)]];
     }
     [self.navigationItem setTitle:@"Profile"];
 }
@@ -62,6 +62,10 @@
     UIStoryboard *mMS=[UIStoryboard storyboardWithName:@"Main" bundle:nil];
     PostViewController *mPVC=[mMS instantiateViewControllerWithIdentifier:@"PostViewController"];
     [self.navigationController pushViewController:mPVC animated:YES];
+}
+-(void)viewPostsSelected
+{
+    
 }
 -(void)saveButtonSelected
 {
@@ -77,6 +81,7 @@
             
             if (succeeded){
                 [[NSUserDefaults standardUserDefaults] setObject:_mNameTF.text forKey:@"Name"];
+                
                 [self.view setUserInteractionEnabled:NO];
                 [self postButtonClicked];
             }
